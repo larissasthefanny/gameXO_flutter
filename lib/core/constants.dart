25a7b7fea36036c89d24055c45fe699f554b8a06
larissasthefanny_ <70843318+larissasthefanny@users.noreply.github.com>
2021-06-08 09:31:23 -0300

    Initial commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Color(0xffac58a4);
+const Color PLAYER2_COLOR = Colors.blue;
+
+const String PLAYER1_SYMBOL = 'X';
+const String PLAYER2_SYMBOL = 'O';
+
+const String GAME_TITLE = 'Jogo X.X';
+const String TIED_TITLE = 'Game Tied!';
+const String WIN_TITLE = 'Player [SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Press the reset button to start again!';
+const String SINGLE_PLAYER_MODE_LABEL = 'Single Player';
+const String MULTIPLAYER_MODE_LABEL = 'Two Players';
+const String RESET_BUTTON_LABEL = 'RESET';
---
+++ lib/pages/game_page.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/controllers/game_controller.dart';
+import 'package:tictactoe/core/constants.dart';
+import 'package:tictactoe/enums/player_type.dart';
+import 'package:tictactoe/enums/winner_type.dart';
+import 'package:tictactoe/widgets/custom_dialog.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final _controller = GameController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text('X.O'),
+      centerTitle: true,
+    );
+  }
+
+  _buildBody() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _buildBoard(),
+          _buildPlayerMode(),
+          _buildResetButton(),
+        ],
+      ),
+    );
+  }
+
+  _buildResetButton() {
+    return RaisedButton(
+      padding: const EdgeInsets.all(20),
+      child: Text(RESET_BUTTON_LABEL),
+      onPressed: _onResetGame,
+    );
+  }
+
+  _buildBoard() {
+    return Expanded(
+      child: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: BOARD_SIZE,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        itemBuilder: _buildTile,
+      ),
+    );
+  }
+
+  Widget _buildTile(context, index) {
+    return GestureDetector(
+      onTap: () => _onMarkTile(index),
+      child: Container(
+        color: _controller.tiles[index].color,
+        child: Center(
+          child: Text(
+            _controller.tiles[index].symbol,
+            style: TextStyle(
+              fontSize: 72.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _onResetGame() {
+    setState(() {
+      _controller.reset();
+    });
+  }
+
+  _onMarkTile(index) {
+    if (!_controller.tiles[index].enable) return;
+
+    setState(() {
+      _controller.markBoardTileByIndex(index);
+    });
+
+    _checkWinner();
+  }
+
+  _checkWinner() {
+    var winner = _controller.checkWinner();
+    if (winner == WinnerType.none) {
+      if (!_controller.hasMoves) {
+        _showTiedDialog();
+      } else if (_controller.isSinglePlayer &&
+          _controller.currentPlayer == PlayerType.player2) {
+        final index = _controller.automaticMove();
+        _onMarkTile(index);
+      }
+    } else {
+      String symbol =
+          winner == WinnerType.player1 ? PLAYER1_SYMBOL : PLAYER2_SYMBOL;
+      _showWinnerDialog(symbol);
+    }
+  }
+
+  _showWinnerDialog(String symbol) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return CustomDialog(
+          title: WIN_TITLE.replaceAll('[SYMBOL]', symbol),
+          message: DIALOG_MESSAGE,
+          onPressed: _onResetGame,
+        );
+      },
+    );
+  }
+
+  _showTiedDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return CustomDialog(
+          title: TIED_TITLE,
+          message: DIALOG_MESSAGE,
+          onPressed: _onResetGame,
+        );
+      },
+    );
+  }
+
+  _buildPlayerMode() {
+    return SwitchListTile(
+      title: Text(_controller.isSinglePlayer ? 'Single Player' : 'Two Players'),
+      secondary: Icon(_controller.isSinglePlayer ? Icons.person : Icons.group),
+      value: _controller.isSinglePlayer,
+      onChanged: (value) {
+        setState(() {
+          _controller.isSinglePlayer = value;
+        });
+      },
+    );
+  }
+}
